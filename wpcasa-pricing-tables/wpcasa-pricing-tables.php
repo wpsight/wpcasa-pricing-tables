bd7819a1ea22d68e7e67a6dc182862b48745876d
Simon Rimkus <simon@wpsight.com>
2016-08-15 14:36:04 +0200

    Tested up to WordPress 4.6

--- wpcasa-pricing-tables/wpcasa-pricing-tables.php
+++ wpcasa-pricing-tables/wpcasa-pricing-tables.php
@@ -7,7 +7,7 @@ Version: 1.0.1
 Author: WPSight
 Author URI: http://wpsight.com
 Requires at least: 4.0
-Tested up to: 4.5.2
+Tested up to: 4.6
 Text Domain: wpcasa-pricing-tables
 Domain Path: /languages
 
